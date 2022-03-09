6c824904492aebebbba7c031015824766ada6ec1
Brama Udi <bramaudi.al@gmail.com>
2022-03-09 17:20:27 +0700

    Add build signed APK script

---
+++ sign.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+./gradlew assembleRelease \
+-Pandroid.injected.signing.store.file=$KEYFILE \
+-Pandroid.injected.signing.store.password=$STORE_PASSWORD \
+-Pandroid.injected.signing.key.alias=$KEY_ALIAS \
+-Pandroid.injected.signing.key.password=$KEY_PASSWORD
