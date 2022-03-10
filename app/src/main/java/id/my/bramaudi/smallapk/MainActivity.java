4a009a1b8cfc218e889294a4ff2334030f1680c9
Brama Udi <bramaudi.al@gmail.com>
2022-03-10 12:27:31 +0700

    Minimal webview settings

--- app/src/main/java/id/my/bramaudi/smallapk/MainActivity.java
+++ app/src/main/java/id/my/bramaudi/smallapk/MainActivity.java
@@ -1,13 +1,32 @@
 package id.my.bramaudi.smallapk;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 public class MainActivity extends Activity {
+
+    private WebView browser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        browser = (WebView) findViewById(R.id.webview);
+        browserSettings();
+        browser.loadUrl("https://qrman.vercel.app");
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private void browserSettings() {
+        browser.getSettings().setAppCacheEnabled(true);
+        browser.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
+        browser.getSettings().setJavaScriptEnabled(true);
+        browser.getSettings().setDomStorageEnabled(true);
+        browser.getSettings().setAllowFileAccess(true);
+        browser.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
     }
 }
